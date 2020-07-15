5428537896317f5f81508981503c79a27ebf7e61
KRAILERKM <krailerk_man@hotmail.com>
2020-07-15 17:42:50 +0700

    15-07-2020-1742 Update f5cjsonp1

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ from lib.f5cjsonp1 import f5cjsonp1
 
 fileConf = open("tmp\input.conf", "r")
 app1 = f5cjsonp1(fileConf.read())
-app1.genTMSH()
+fileConf.close()
+
+listchangeStyle = app1.genTMSH()
+
+fileConf = open("tmp\output.txt", "w")
+alltxt = str()
+for v1 in listchangeStyle:
+    fileConf.write(v1 + "\n")
+    print(v1)
+fileConf.close()
